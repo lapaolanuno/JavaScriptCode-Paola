8771ac4a22d2dfd3759409439dffd3ddce2797e5
Lapalapa <128613587+lapaolanuno@users.noreply.github.com>
2023-05-24 09:08:45 -0600

    condicionales

--- JavasS/js/main.js
+++ JavasS/js/main.js
@@ -3,7 +3,7 @@ alert("Bienvenido a JavaScript");
 /*let nombre=prompt("cual es tu nombre?");
 comsole.log(nombre)
 console.log(parseInt(nombre))
-console/log(typeof(nombre))*/
+console/log(typeof(nombre))
 let numerouno=prompt("ingresar un numero");
 let numerodos=prompt("ingresa otro numero");
 
@@ -14,4 +14,27 @@ let resultadoSuma=parseInt(numerouno)+ parseInt(numerodos);
         console.log(`el resultado de la suma es: ${resultadoSuma}`);
         console.log(`el resultado de la resta es: ${resultadoResta}`);
         console.log(`el resultado de la multiplicacion es: ${resultadoMulti}`);
+
         console.log(`el resultado de la division es: ${resultadodiv}`);
+        //Datos dos numeros identificar cual numero es mayor,
+        //cual es menor o si son iguales.
+
+let numerouno= prompt("Ingresa un numero");
+let numerodos= prompt("Ingresa otro numero");
+
+if(numerouno>numerodos){
+        console.log(`El numero ${numerouno} es mayor que ${numerodos}`);
+} else if(numerodos>numerouno){
+        console.log(`El numero ${numerodos} es mayor que ${numerouno}`);
+}else{
+        console.log(`El numero ${numerouno} es igual a ${numerodos}`);
+}*/
+
+//identificar el numero par o inpar
+let numero= prompt ("Ingresa el numero");
+
+if (numero%2==0){
+ console.log(`El numero ${numero} es par`);
+}else{
+        console.log(`el numero ${numero} es impar`)
+}
